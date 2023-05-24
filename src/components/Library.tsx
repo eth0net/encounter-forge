26e5c041c39403b2c490c8e606270fa0a799b2da
Elliot Thomas <eth0net@pm.me>
2023-05-24 17:36:20 +0100

    paginate library

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -1,19 +1,36 @@
+import { Table, TableContainer, TablePagination } from '@mui/material';
+import { useMemo, useState } from "react";
 import Encounter from "../models/Encounter";
 import MonsterData from "../models/MonsterData";
 import LibraryBody from './LibraryBody';
-import { Table, TableContainer } from '@mui/material';
-import Section from './Section';
 import LibraryHead from './LibraryHead';
+import Section from './Section';
 
-export function Library(props: LibraryProps) {
+export function Library({ monsterData, ...props }: LibraryProps) {
+  const [page, setPage] = useState(0);
+  const rowsPerPage = 10;
+
+  const visibleData = useMemo(() => monsterData.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  ), [monsterData, page, rowsPerPage]);
+
   return (
     <Section title='Library'>
       <TableContainer>
         <Table>
           <LibraryHead />
-          <LibraryBody {...props} />
+          <LibraryBody monsterData={visibleData} {...props} />
         </Table>
       </TableContainer>
+      <TablePagination
+        component='div'
+        count={monsterData.length}
+        rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={[rowsPerPage]}
+        page={page}
+        onPageChange={(_, page) => setPage(page)}
+      />
     </Section>
   );
 }
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,10 +2,10 @@ import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
-export function Section({ title, children }: SectionProps) {
+export function Section({ title, children, paperProps, stackProps }: SectionProps) {
   return (
-    <Paper elevation={4}>
-      <Stack spacing={1} p={2}>
+    <Paper elevation={4} {...paperProps}>
+      <Stack spacing={1} p={2} {...stackProps}>
         <Typography variant='h5'>{title}</Typography>
         {children}
       </Stack>
@@ -16,6 +16,8 @@ export function Section({ title, children }: SectionProps) {
 export interface SectionProps {
   title: string;
   children: React.ReactNode;
+  paperProps?: React.ComponentProps<typeof Paper>;
+  stackProps?: React.ComponentProps<typeof Stack>;
 }
 
 export default Section;
