f8a5cfd059c1c27bcab7af682d393d7750c2b903
Elliot Thomas <eth0net@pm.me>
2023-05-24 16:52:25 +0100

    calculate xp

--- src/hooks/useMonsterData.tsx
+++ src/hooks/useMonsterData.tsx
@@ -3,11 +3,11 @@ import MonsterData from "../models/MonsterData";
 
 export function useMonsterData() {
   const monsterData: MonsterData = [
-    new Monster('Goblin', 0.25, 50),
-    new Monster('Orc', 0.5, 100),
-    new Monster('Ogre', 2, 450),
-    new Monster('Troll', 5, 1800),
-    new Monster('Dragon', 15, 13000),
+    new Monster('Goblin', 0.25),
+    new Monster('Orc', 0.5),
+    new Monster('Ogre', 2),
+    new Monster('Troll', 5),
+    new Monster('Dragon', 15),
   ];
 
   return monsterData;
